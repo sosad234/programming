311b2b09c465884afcd103594aa076bab0b5d085
user <user@ws1013402134.pm>
2023-10-02 16:13:16 +0500

    wip lab02

--- lab02/prog.c
+++ lab02/prog.c
@@ -4,16 +4,20 @@
 
 int main()
 {
-    double x,s,res;
-    x = 0.0;
     
-    scanf("%lf",&s);    
-    while(x <= 3)
+    double x,h,res;
+    x = 0.0;
+    int z; 
+    scanf("%lf", &h);
+    z = 3 / h + 1;  
+    while(z)
     {
         if (x >= 0 && x <= 1.5)
-        res =  pow(2,x) - 2 + x*x;
-        if(x <= 1.5 && x <= 3)
-        res = sqrt(x) * exp(pow(-x, 2));
-        printf(" %lf %lf/n", x, res);
+            res =  pow(2,x) - 2 + x*x;
+        if(x > 1.5 && x <= 3.0)
+            res = sqrt(x) * exp(-x * x);
+        printf(" %lf %lf\n", x, res);
+        x += h;
+        z = z - 1;
     }
 }
