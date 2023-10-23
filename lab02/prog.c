5553175ee020150ec285bb442cff0680bdb2221c
user <user@ws1013402134.pm>
2023-10-23 16:05:53 +0500

    wap lab02

--- lab02/prog.c
+++ lab02/prog.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <math.h> 
-
+#include <unistd.h>
 
 int main()
 {
     
     double x,h,res;
     x = 0.0;
-    int z; 
     scanf("%lf", &h);
-    z = 3 / h + 1;  
-    while(z)
+    int z;
+    int k;
+    k = 0;
+    z = 3 / h + 1; 
+    
+    while(z > k)
     {
         if (x >= 0 && x <= 1.5)
-            res =  pow(2,x) - 2 + x*x;
-        if(x > 1.5 && x <= 3.0)
+            res =  pow(2,x) - 2 + pow(x, 2);
+        if (x > 1.5 && x <= 3)
             res = sqrt(x) * exp(-x * x);
-        printf(" %lf %lf\n", x, res);
+        printf("%lf %lf\n", x, res);
         x += h;
-        z = z - 1;
+        k++;
     }
+    return 0;
 }
