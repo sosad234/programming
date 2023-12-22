277ce6256c0f81986e06b4c7031f71641c37d6d5
sosad234 <olegposh2005@gmail.com>
2023-12-23 01:09:32 +0300

    wip lab11

--- lab11/5v.py
+++ lab11/5v.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import datetime
+import matplotlib.pyplot as plt
 
 def add_expense():
     expense = entry_expense.get()
