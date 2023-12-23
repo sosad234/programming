29a3d9bda4f9b507bfba7b1c6f39c67d8df81c30
kzar00 <kzar000@mail.ru>
2023-12-23 17:27:02 +0500

    wip lab05

--- lab05/5v2.py
+++ lab05/5v2.py
@@ -1,32 +1,39 @@
-import math
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-def f(x):
-    return math.sqrt(x) * math.exp(-x**2)
-def f_pr(x):
-    return (-4*x*x+1/(2*math.exp(x**2)*x**(1/2)))
-def yk(t, x):
-    for i in x:
-        y1.append(f(t) + f_pr(t) * (i - t))
-    return y1
-tochka_kasaniya = 0.5
-h = float(input())
-h = round(1/h)
-x = np.linspace(1.5,3,h)
-y = []
-for i in x:
-    y.append(f(i))
-x1 = [1.5, 3.0]
-y1 =[]
-plt.title('График')
-plt.xlabel('x') 
-plt.ylabel('y') 
-plt.grid()
-plt.plot(x, y)
-plt.plot(x1, yk(tochka_kasaniya, x1))
-plt.plot(tochka_kasaniya, f(tochka_kasaniya), "ro")
+# Определим функцию y = sqrt(x) * exp(-x**2)
+def func(x):
+    return np.sqrt(x) * np.exp(-x**2)
 
+# Зададим диапазон значений x
+x = np.linspace(0, 2, 100)  # от 0 до 2 с шагом 0.01
+
+# Построим график функции
+plt.plot(x, func(x), label='y = sqrt(x) * exp(-x**2)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+plt.legend()
+
+# Найдем производную функции для определения наклона касательной
+def derivative(x):
+    return (1 - 2*x**2) * np.exp(-x**2) / (2*np.sqrt(x))
+
+# Найдем значение производной в точке x=1
+slope = derivative(1)
+
+# Найдем значение функции в точке x=1
+y_val = func(1)
+
+# Уравнение касательной
+tangent_line = lambda xx : slope*(xx - 1) + y_val
+
+# Построим уравнения касательной
+plt.plot(x, tangent_line(x), label='Tangent at x=1', linestyle='--')
+plt.scatter(1, y_val, color='red')  # отметим точку касания))
+
+plt.title('График функции с касательной')
+plt.legend()
 plt.show()
 
 
